007829a3742a22925b5f4341f7a54bf6f4c7287c
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-03-24 14:54:23 +0900

    文字種類制限の追加　2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name,       presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name,      presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana,  presence: true,format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶ]+\z/ }
   validates :birth_date,      presence: true
 
   validates :password,        presence: true,length: { minimum: 6 },format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
