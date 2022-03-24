cd279c705e98e6d2488b176d1273d0283858b212
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-03-25 00:20:49 +0900

    ユーザー管理機能の修正　2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :first_name,      presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :last_name_kana,  presence: true,format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :birth_date,      presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
 
-  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
+      validates :password
+    end
+  end
 end
