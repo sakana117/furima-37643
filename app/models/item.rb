d866342aab8109a9ea1ec46427406bfa0a6bed45
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-03-28 04:42:51 +0900

    エラーメッセージの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,17 @@
 class Item < ApplicationRecord
   with_options presence: true do
-    validates :item_name
-    validates :item_info
-    validates :item_price
     validates :image
+    validates :item_name,length: { maximum: 40 }
+    validates :item_info,length: { maximum: 1000 }
+    validates :item_price,numericality: { in: 300..9999999, message:"is out of setting range" },format: { with: /\A[0-9]+\z/,message:"is invalid. Input half-width characters" }
+  end
 
-    with_options numericality: { other_than: 1 , message: "can't be blank"} do
-      validates :category_id
-      validates :sales_status_id
-      validates :shipping_fee_status_id
-      validates :prefecture_id
-      validates :scheduled_delivery_id
-    end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
   end
 
   has_one_attached :image
