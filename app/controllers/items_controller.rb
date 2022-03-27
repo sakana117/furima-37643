45e17927a899222f9cbbb5db596953bd440480b9
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-03-27 19:19:07 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,12 +3,26 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item=Item.new
   end
 
   def create
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name,:item_info,
+                                 :category_id,:sales_status_id,:shipping_fee_status_id,:prefecture_id,
+                                 :scheduled_delivery_id,:item_price,:imege)
+                                 .merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,10 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
     validates :item_price
-    validates :user, foreign_key:true
     validates :image
   end
 
   has_one_attached :image
-  belongs :user
+  belongs_to :user
 
 end
