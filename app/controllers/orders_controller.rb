629ae3f4216a652fd3eb1ff5527d4e49a9fb94ef
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-05 20:02:00 +0900

    商品の購入状況によるビューの切り替え

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    #binding.pry
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
