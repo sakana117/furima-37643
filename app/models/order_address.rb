b7973bb562ec36ed6a231afccc7b80bb8771efa4
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-05 09:48:31 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,12 +6,15 @@ class AddressAddress
     validates :item_id
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
     validates :city
     validates :addresses
     validates :building
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid."}
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/}
     validates :order_id
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :prefecture
+
+
 end
