e71b07d340c7b4211e777110b411084788db221d
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:27:24 +0900

    Revert "Revert "テストコードの作成""
    This reverts commit 96871b30c2084569172e9e518d564b9dd54f705f.

--- spec/factories/orders_addresses.rb
+++ spec/factories/orders_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
-  factory :order do
-    
+  factory :order_address do
+    postal_code             {'123-4567'}
+    prefecture_id           {Faker::Number.between(from: 2, to: 11)}
+    city                    {Faker::Address.city}
+    addresses               {Faker::Address.street_address}
+    building                {Faker::Address.building_number}
+    phone_number            {Faker::Number.number(digits: 11)}
   end
 end
