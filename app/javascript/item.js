c0c91130d573759e1fc3deeb269349b2b2bdfe69
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:23:49 +0900

    Revert "JavaScriptの作成・最適化"
    This reverts commit e66ff75ca3e274a1d82fc7e7f1eff0e552c76308.

--- app/javascript/item.js
+++ app/javascript/item.js
@@ -1,6 +1,4 @@
-if(document.URL.match(/new/)){
-  window.addEventListener("load", item);
-}
+window.addEventListener("load", item);
 
 function item (){
   const ItemPrice = document.getElementById("item-price");
@@ -14,18 +12,5 @@ function item (){
 
     const Profit = document.getElementById("profit");
     Profit.innerHTML = formatter.format(inputItemPrice-inputAddTaxDom);
-  });
-}
-
-if(document.URL.match(/orders/)){
-  window.addEventListener("load", pay);
-}
-
-function pay (){
-  Payjp.setPublicKey("pk_test_******************"); // PAY.JPテスト公開鍵
-  const submit = document.getElementById("button");
-  submit.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
   });
 }
