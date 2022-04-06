f8d23aa1ed3a050f69b30343a957a85557fe3371
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:27:46 +0900

    Revert "Revert "テストコードの追加作成""
    This reverts commit 6d88fb5824d078ed549f56b9f045abd0d3bfd7cf.

--- spec/factories/orders_addresses.rb
+++ spec/factories/orders_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     addresses               {Faker::Address.street_address}
     building                {Faker::Address.building_number}
     phone_number            {Faker::Number.number(digits: 11)}
+    token                   {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -92,6 +92,13 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
+
+      #新規登録できないとき：token関連：1個
+      it 'tokenが空では登録できない' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
