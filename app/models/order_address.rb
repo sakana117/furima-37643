a98e14b046c1cda6fc4272869ea01ebe89f8e76f
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 09:09:58 +0900

    テストコードの作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,6 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :addresses
-    validates :building
     validates :phone_number, format: {with: /\A[0-9]{11}\z/}
   end
   
--- spec/models/order_addresses_spec.rb
+++ spec/models/order_addresses_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe OrderAddress, type: :model do
+  before do
+    item = FactoryBot.create(:item)
+    user = FactoryBot.create(:user)
+    @order_address = FactoryBot.build(:order_address, item_id: item.id, user_id: user.id)
+   sleep(1)
+  end
+
+  describe 'ユーザー新規登録' do
+    #テスト総数：13個
+    context '新規登録できるとき' do
+      #新規登録できるとき：2個
+      it '全ての項目が存在すれば登録できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingが空欄でも登録できる' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '新規登録できないとき' do
+      #新規登録できないとき：11個
+      #新規登録できないとき：住所関連：7個
+      it 'postal_codeが空では登録できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it 'postal_codeが○◯○-○◯○○の形でなければ登録できない(○は全て半角数字)' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
+      it 'cityが空では登録できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+
+      it 'addressesが空では登録できない' do
+        @order_address.addresses = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Addresses can't be blank")
+      end
+
+      it 'phone_numberが空では登録できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'phone_numberが12文字以上では登録できない' do
+        @order_address.phone_number = Faker::Number.number(digits: 12)
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it 'phone_numberが10文字以下では登録できない' do
+        @order_address.phone_number = Faker::Number.number(digits: 10)
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      #新規登録できないとき：ActiveHash関連：2個
+      it 'prefecture_idが空では登録できない' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'prefecture_idが"1"では登録できない' do
+        @order_address.prefecture_id = '1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      #新規登録できないとき：user関連：1個
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      #新規登録できないとき：item関連：1個
+      it 'アイテムが紐付いていなければ投稿できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
 end
