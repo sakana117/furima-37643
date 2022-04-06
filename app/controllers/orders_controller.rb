6a15a56bbb252b66ead7694c51d05f7b78dc0885
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:23:43 +0900

    Revert "JavaScript_tokenバリデーション追加"
    This reverts commit 0086b86701a6fe6634daf76a16db3aa42de67e4f.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,6 +25,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id,
                                           :city, :addresses, :building, :phone_number, :order_id)
-                                          .merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+                                          .merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
