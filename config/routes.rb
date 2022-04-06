e07550e9565218b31e9a3a2907c600636d5ffb8b
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:24:08 +0900

    Revert "ルーティングのネストを作成"
    This reverts commit ce2139dbbaec1271531d7759320780eba2b43d0e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items do
-    resources :orders ,only: [:index, :new, :create]
-  end
+  resources :items
+  resources :orders ,only: [:index, :new, :create]
 end
