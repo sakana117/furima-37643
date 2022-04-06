e78cf44e3e5b5eeca2ba0e386d63ff1341111ef2
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:23:57 +0900

    Revert "商品の購入状況によるページの遷移設定"
    This reverts commit 501932c6f69f5172d2cd622a21fefa7584d6d290.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only:[:index]
-
   def index
     @item = Item.find(params[:item_id])
-    if Order.where(item_id: @item.id).exists? || current_user.id == @item.user_id
-      redirect_to root_path
-    end
     @order_address = OrderAddress.new
   end
 
