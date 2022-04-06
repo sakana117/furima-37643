1d1725024ef1a75a3b34c658e8b0fc68251004ec
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-06 14:26:51 +0900

    Revert "Revert "ルーティングのネストを作成""
    This reverts commit e07550e9565218b31e9a3a2907c600636d5ffb8b.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,12 @@
 class OrdersController < ApplicationController
   def index
+    #@order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
-  def new
-    @order_address = OrderAddress.new
-  end
+  #def new
+    #@order_address = OrderAddress.new
+  #end
 
   def create
     @order_address = OrderAddress.new(order_params)
@@ -21,6 +23,6 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address).permit(:postal_code, :prefecture_id,
                                           :city, :addresses, :building, :phone_number, :order_id)
-                                          .merge(user_id: current_user.id)
+                                          .merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
