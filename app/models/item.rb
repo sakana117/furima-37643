918480fdb26f81ba1ca78a887d2d440a32648ca2
sakana117 <100276705+sakana117@users.noreply.github.com>
2022-04-07 10:14:57 +0900

    本番環境修正_item_priceエラーメッセージ

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
     validates :item_name  ,length: { maximum: 40 }
     validates :item_info  ,length: { maximum: 1000 }
     validates :item_price ,numericality: { with: /\A[0-9]+\z/,message:"is invalid. Input half-width characters" }
-    validates :item_price ,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-    
   end
+
+  validates :item_price ,numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
